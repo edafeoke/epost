5635358a4a0ba6fe1feb0c6bcca5b76bb9c70873
Oke Edafe Great <greatedafeoke@gmail.com>
2023-06-05 13:06:41 -0700

    Added some files

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail, Message
+from flask_login import LoginManager, login_user, login_required, logout_user
+from models import *
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 mail = Mail(app)
+login_manager = LoginManager(app)
 
-# Define your models in models.py and import them here
+# Define your User model in models.py and import it here
+
+# Configure Flask-Login
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    # Implement the user loader function based on your User model
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/inbox', methods=['POST'])
+@app.route('/inbox', methods=['POST', 'GET'])
+@login_required
 def inbox():
+    # Handle inbox route for authenticated users only
     return render_template('inbox.html')
 
 @app.route('/compose', methods=['GET', 'POST'])
+@login_required
 def compose():
     if request.method == 'POST':
         recipient = request.form['recipient']
@@ -31,6 +45,50 @@ def compose():
     return render_template('compose.html')
 
 # Add your routes for registration, login, and other functionality
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        # Validate the login credentials and login the user
+        user = User.query.filter_by(email=email).first()
+        if user and user.check_password(password):
+            login_user(user)
+            return jsonify({'message': 'Logged in successfully'})
+        else:
+            return jsonify({'message': 'Invalid email or password'})
+    return render_template('login.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        full_name = request.form['full_name']
+
+        # Check if the user already exists
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists. Please login.')
+            return redirect(url_for('login'))
+
+        # Create a new user
+        new_user = User(email=email, password=password, full_name=full_name)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Registration successful. Please login.')
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message': 'Logged out successfully'})
+
+# Run the application
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,3 +1,34 @@
 from app import db
+from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-# Define your models using SQLAlchemy
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    full_name = db.Column(db.String(120), nullable=False)
+
+    def __init__(self, email, password, full_name):
+        self.email = email
+        self.password_hash = generate_password_hash(password)
+        self.full_name = full_name
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+
+class Email(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(200), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', backref=db.backref('emails', lazy=True))
+
+    def __init__(self, subject, body, user):
+        self.subject = subject
+        self.body = body
+        self.user = user
