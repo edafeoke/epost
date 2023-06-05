6313252258ee1d040e0c5029237f961d42ca9dc1
Oke Edafe Great <greatedafeoke@gmail.com>
2023-06-05 11:12:01 -0700

    Updated code

--- app.py
+++ app.py
@@ -1,89 +1,36 @@
 from flask import Flask, render_template, request, jsonify
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
-app.config['DATABASE'] = 'emails.db'
+app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
+mail = Mail(app)
 
-def get_db_connection():
-    conn = sqlite3.connect(app.config['DATABASE'])
-    conn.row_factory = sqlite3.Row
-    return conn
-
-def create_email_table():
-    conn = get_db_connection()
-    conn.execute('''
-        CREATE TABLE IF NOT EXISTS emails (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            sender TEXT NOT NULL,
-            recipient TEXT NOT NULL,
-            subject TEXT NOT NULL,
-            body TEXT NOT NULL,
-            status TEXT NOT NULL DEFAULT 'unread',
-            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-        )
-    ''')
-    conn.commit()
-    conn.close()
+# Define your models in models.py and import them here
 
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    emails = conn.execute('SELECT * FROM emails').fetchall()
-    conn.close()
-    return render_template('index.html', emails=emails)
+    return render_template('index.html')
+
+@app.route('/inbox', methods=['POST'])
+def inbox():
+    return render_template('inbox.html')
 
 @app.route('/compose', methods=['GET', 'POST'])
 def compose():
     if request.method == 'POST':
-        sender = request.form['sender']
         recipient = request.form['recipient']
         subject = request.form['subject']
         body = request.form['body']
-
-        conn = get_db_connection()
-        conn.execute('INSERT INTO emails (sender, recipient, subject, body) VALUES (?, ?, ?, ?)',
-                     (sender, recipient, subject, body))
-        conn.commit()
-        conn.close()
-
-        return jsonify({'message': 'Email sent successfully.'})
-    
+        # Send the email using Flask-Mail
+        msg = Message(subject, recipients=[recipient])
+        msg.body = body
+        mail.send(msg)
+        return jsonify({'message': 'Email sent successfully'})
     return render_template('compose.html')
 
-@app.route('/inbox', methods=['GET', 'POST'])
-def compose():
-    if request.method == 'POST':
-        # sender = request.form['sender']
-        # recipient = request.form['recipient']
-        # subject = request.form['subject']
-        # body = request.form['body']
-
-        # conn = get_db_connection()
-        # conn.execute('INSERT INTO emails (sender, recipient, subject, body) VALUES (?, ?, ?, ?)',
-        #              (sender, recipient, subject, body))
-        # conn.commit()
-        # conn.close()
-
-        return jsonify({'message': 'Email sent successfully.'})
-    
-    return render_template('inbox.html')
-
-
-@app.route('/email/<int:email_id>')
-def email(email_id):
-    conn = get_db_connection()
-    email = conn.execute('SELECT * FROM emails WHERE id = ?', (email_id,)).fetchone()
-    conn.close()
-    return render_template('email.html', email=email)
-
-@app.route('/delete/<int:email_id>', methods=['POST'])
-def delete(email_id):
-    conn = get_db_connection()
-    conn.execute('DELETE FROM emails WHERE id = ?', (email_id,))
-    conn.commit()
-    conn.close()
-    return jsonify({'message': 'Email deleted successfully.'})
+# Add your routes for registration, login, and other functionality
 
 if __name__ == '__main__':
-    create_email_table()
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,6 +1,12 @@
-import os
+# Flask configuration
+SECRET_KEY = 'your-secret-key'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/epost'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'mysql://username:password@localhost/db_name'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+# Flask-Mail configuration
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'greatedafeoke@gmail.com'
+MAIL_PASSWORD = 'your-password'
+MAIL_DEFAULT_SENDER = 'greatedafeoke@gmail.com'
